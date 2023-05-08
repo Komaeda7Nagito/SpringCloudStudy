4b0aa683dfb6d7e1973077bac025c9c0a0da9436
KomaedaNagito <Komaeda_hope@outlook.com>
2023-05-08 23:21:38 +0800

    微服务入门

---
+++ book-service/src/main/java/com/xzzzf/controller/BookController.java
@@ -0,0 +1,21 @@
+package com.xzzzf.controller;
+
+import com.xzzzf.entity.Book;
+import com.xzzzf.service.BookService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class BookController {
+
+    @Resource
+    private BookService bookService;
+
+    @RequestMapping("/book/{bid}")
+    public Book findUserById(@PathVariable("bid") Integer bid) {
+        return bookService.getBookById(bid);
+    }
+}
